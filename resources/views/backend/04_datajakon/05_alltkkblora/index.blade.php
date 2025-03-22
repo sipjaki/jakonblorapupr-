c1752d0fd051bd2f7a0aace601b233f2d31c997a
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-23 05:02:26 +0700

    er

--- resources/views/backend/04_datajakon/05_alltkkblora/index.blade.php
+++ resources/views/backend/04_datajakon/05_alltkkblora/index.blade.php
@@ -80,24 +80,21 @@
                      <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
 
                         <a href="/404">
-                            <button
-                                onmouseover="this.style.color='black';"
-                                onmouseout="this.style.color='white';"
-                                style="background: linear-gradient(135deg, #2C3E50, #5D9CEC);  color: white; border: none; margin-right: 10px; padding: 10px 20px;
-                                       border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center;
-                                       transition: color 0.3s; text-decoration: none;">
-                                <!-- Ikon Kembali -->
-                                <i class="fa fa-database" style="margin-right: 8px;"></i>
-                                Universitas/Sekolah/Instansi
-                            </button>
-                        </a>
-
+                             <button
+                             onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
+                             onmouseout="this.style.backgroundColor='#1a2a3a'; this.style.color='white';"
+                             style="background-color: #1a2a3a; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                             <!-- Ikon Kembali -->
+                             <i class="fa fa-database" style="margin-right: 8px;"></i>
+                             Universitas/Sekolah/Instansi
+                         </button>
+                         </a>
 
                          <a href="/404">
                              <button
                              onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                             onmouseout="this.style.backgroundColor='#166534'; this.style.color='white';"
-                             style="background: linear-gradient(135deg, #2C3E50, #5D9CEC);  color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                             onmouseout="this.style.backgroundColor='#1a2a3a'; this.style.color='white';"
+                             style="background-color: #1a2a3a; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
                              <!-- Ikon Kembali -->
                              <i class="fa fa-database" style="margin-right: 8px;"></i>
                              Jenjang Pendidikan
