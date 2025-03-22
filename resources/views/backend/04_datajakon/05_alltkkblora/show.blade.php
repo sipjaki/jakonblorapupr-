79b44dac858a9f00fc54eb02ed427696955bc361
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-23 04:38:30 +0700

    dfg

--- resources/views/backend/04_datajakon/05_alltkkblora/show.blade.php
+++ resources/views/backend/04_datajakon/05_alltkkblora/show.blade.php
@@ -46,14 +46,18 @@
                 <!-- /.card -->
                 <div class="card mb-4">
                     <div class="card-header">
-                        <h2 style="color: black;" class="card-title">Data Details : <button class="btn btn-success" style="background-color: #1d643b; border-color: #1d643b; font-weight: bold;"
-                            onmouseover="this.style.backgroundColor='#ffffff'; this.style.color='#000000'; this.style.borderColor='#1d643b';"
-                            onmouseout="this.style.backgroundColor='#1d643b'; this.style.color='#ffffff'; this.style.borderColor='#1d643b';">
-                            {{ $data->nama }}
-                        </button>
+                        <h2 class="card-title" style="color: black;">
+                            Data Details :
+                            <button class="btn btn-success"
+                                    style="background-color: #1d643b; border-color: #1d643b; font-weight: bold; padding: 10px 20px;
+                                           border-radius: 5px; font-size: 16px;"
+                                    onmouseover="this.style.backgroundColor='#ffffff'; this.style.color='#000000'; this.style.borderColor='#1d643b';"
+                                    onmouseout="this.style.backgroundColor='#1d643b'; this.style.color='#ffffff'; this.style.borderColor='#1d643b';">
+                                {{ $data->nama }}
+                            </button>
                         </h2>
 
-                            <button id="status-{{ $data->id }}" class="btn btn-sm"></button>
+                        <button id="status-{{ $data->id }}" class="btn btn-sm" style="padding: 10px 20px; border-radius: 5px; font-size: 14px;"></button>
 
                         <script>
                             function updateStatus() {
@@ -76,7 +80,6 @@
                             // Update setiap 1 detik untuk realtime
                             setInterval(updateStatus, 1000);
                         </script>
-
                     </div>
 
                     <div class="col-md-12">
