f7251812c4759130f71f096da9022f6920b60dfb
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-23 04:53:41 +0700

    oerubahan barulagi

--- resources/views/backend/04_datajakon/05_alltkkblora/index.blade.php
+++ resources/views/backend/04_datajakon/05_alltkkblora/index.blade.php
@@ -78,7 +78,6 @@
                 </div>
 
                      <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
-
                         <a href="/404">
                             <button
                                 onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
@@ -86,8 +85,8 @@
                                 style="background: linear-gradient(135deg, #2C3E50, #5D9CEC); color: white; border: none; margin-right: 10px;
                                        padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex;
                                        align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
-                                <!-- Ikon Kembali -->
-                                <i class="fa fa-database" style="margin-right: 8px;"></i>
+                                <!-- Ikon dengan tema yang lebih cocok (misalnya ikon rumah atau aplikasi) -->
+                                <i class="fa fa-home" style="margin-right: 8px;"></i> <!-- Ikon rumah sebagai contoh -->
                                 Universitas/Sekolah/Instansi
                             </button>
                         </a>
