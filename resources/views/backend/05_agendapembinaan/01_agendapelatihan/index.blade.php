28b3eef9594966749ed3285fa59b1380286452f0
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-23 20:02:49 +0700

    dfg

--- resources/views/backend/05_agendapembinaan/01_agendapelatihan/index.blade.php
+++ resources/views/backend/05_agendapembinaan/01_agendapelatihan/index.blade.php
@@ -286,7 +286,7 @@
                 <p>PDF cannot be displayed.</p>
             </object>
         </td>
-         <td style="text-align: center; vertical-align: middle; width: 100%; align-items:center; gap:10px;">
+         <td style="text-align: center; vertical-align: middle; width: 100%; align-items:center; gap:10px; margin:left:25px;">
             <a href="{{ url('/beagendapelatihanpeserta/show/' . $item->id) }}" style="text-decoration: none;">
             <button
                 onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
